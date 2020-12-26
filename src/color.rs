ff04266e35e10eae3bd1b4167047d6e105190b46
Oskari Virtaoja <oskari@swarmia.com>
2020-12-26 18:57:25 +0200

    Use Color module to write colors

--- src/color.rs
+++ src/color.rs
@@ -3,7 +3,25 @@ use crate::vec3;
 pub type Color = vec3::Vec3;
 
 impl Color {
+    pub fn new(r: f32, g: f32, b: f32) -> Color {
+        return vec3::Vec3(r, g, b);
+    }
+
+    pub fn r(&self) -> f32 {
+        self.0
+    }
+
+    pub fn g(&self) -> f32 {
+        self.1
+    }
+
+    pub fn b(&self) -> f32 {
+        self.2
+    }
     pub fn write(&self) {
-        println!("{} {} {}", self.0, self.1, self.2)
+        let ir = (255.999 * self.r()) as u32;
+        let ig = (255.999 * self.g()) as u32;
+        let ib = (255.999 * self.b()) as u32;
+        println!("{} {} {}", ir, ig, ib)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 const IMAGE_WIDTH: u32 = 256;
 const IMAGE_HEIGHT: u32 = 256;
 
+mod color;
+mod vec3;
+
 fn main() {
     // Render
     eprintln!("Starting render");
@@ -16,12 +19,11 @@ fn main() {
             let g: f32 = j as f32 / (IMAGE_HEIGHT - 1) as f32;
             let b: f32 = 0.25;
 
-            let ir = (255.999 * r) as u32;
-            let ig = (255.999 * g) as u32;
-            let ib = (255.999 * b) as u32;
-            println!("{} {} {}", ir, ig, ib);
+            color::Color::new(r, g, b).write();
         }
     }
 
+    let foo = vec3::Vec3(1.0, 2.0, 3.0);
+    foo.length();
     eprintln!("Done.");
 }
