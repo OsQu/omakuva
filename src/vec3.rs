060ba3dde1fb8016249d781a3634100ce4185274
Oskari Virtaoja <oskari@swarmia.com>
2020-12-26 16:40:39 +0200

    Add add_assign to vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -11,6 +11,14 @@ impl ops::Neg for Vec3 {
     }
 }
 
+impl ops::AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        self.0 += other.0;
+        self.1 += other.1;
+        self.2 += other.2;
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -21,5 +29,13 @@ mod tests {
         let negated_vector = -vector;
 
         assert_eq!(negated_vector, Vec3(-1.0, -2.0, -3.0))
+    }
+
+    #[test]
+    fn test_add_assign() {
+        let mut vector = Vec3(1.0, 2.0, 3.0);
+        vector += Vec3(1.0, 2.0, 3.0);
+
+        assert_eq!(vector, Vec3(2.0, 4.0, 6.0))
     }
 }
