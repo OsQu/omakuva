38bca0cdc5fd0526b2dd887ca7c95cc5eb4dae30
Oskari Virtaoja <oskari@swarmia.com>
2020-12-26 20:07:22 +0200

    Specify image, viewport and camera dimensions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
-const IMAGE_WIDTH: u32 = 256;
-const IMAGE_HEIGHT: u32 = 256;
+// Image dimensions
+const ASPECT_RATIO: f32 = 16.0 / 9.0;
+const IMAGE_WIDTH: u32 = 400;
+const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f32 / ASPECT_RATIO) as u32;
+
+// Viewport
+const VIEWPORT_HEIGHT: f32 = 2.0;
+const VIEWPORT_WIDTH: f32 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+const FOCAL_LENGTH: f32 = 1.0;
 
 mod color;
 mod ray;
 mod vec3;
 
+use crate::vec3::*;
+
 fn main() {
+    // Camera
+    let origin = Point3::new(0.0, 0.0, 0.0);
+    let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
+    let lower_left_corner =
+        origin - &horizontal / 2.0 - &vertical / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
+
     // Render
     eprintln!("Starting render");
 
