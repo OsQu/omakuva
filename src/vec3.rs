d07dcabbf04f590256b451be390eec76b3098aec
Oskari Virtaoja <oskari@swarmia.com>
2021-01-02 18:34:47 +0200

    Add retract to vec3

--- src/vec3.rs
+++ src/vec3.rs
@@ -66,6 +66,15 @@ impl Vec3 {
         return self - &((2.0 * self.dot(normal)) * normal);
     }
 
+    pub fn retract(&self, normal: &Vec3, etai_over_etat: f32) -> Vec3 {
+        // Calculate r_out using retraction formula
+        let cos_theta = self.dot(normal).min(1.0);
+        let r_out_perp = etai_over_etat * (self + &(cos_theta * normal));
+        let r_out_parallel = (-(1.0 - r_out_perp.length_squared()).abs().sqrt()) * normal;
+
+        return r_out_perp + r_out_parallel;
+    }
+
     pub fn length(&self) -> f32 {
         self.length_squared().sqrt()
     }
